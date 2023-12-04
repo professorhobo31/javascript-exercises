3ef931138f9983e9e0e0abf3305ad99cfca677d9
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2023-12-04 18:41:23 -0300

    Add solution to exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split('');
+    array.reverse();
+    let text = ""
+    for (i=0; i<array.length; i++) {
+        text += array[i];
+    }
+    return text
 };
 
 // Do not edit below this line
