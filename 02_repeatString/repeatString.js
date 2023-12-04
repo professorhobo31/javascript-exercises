a34886e63c9af55ae4801da9fb3dcc754b4f8b95
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2023-12-04 17:25:45 -0300

    Add solution to exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let text = ""
+    if (num < 0) {
+        return "ERROR"
+    }
+    else for (i = 0; i<num; i++) {
+        text += [string]
+    }
+    return text
 };
 
 // Do not edit below this line
