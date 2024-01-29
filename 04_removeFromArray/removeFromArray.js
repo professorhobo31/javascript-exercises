3e8a8a4c2fe9fe6b91498c7a5aed2eb83f38544d
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-01-29 14:18:35 -0300

    Return from vacation, change to delete and filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, number = "", number2 = "", number3 = "") {
+    if (number2 == "") {
+        const arrayLength = inputArray.length
+        // array.splice(number - 1, 1)
+        delete inputArray[number - 1]
+        let array = inputArray.filter(Boolean)
+        return array
+    }
+    if (number3 == "") {
+        // const arrayLength = inputArray.length
+        // array.splice(number - 1, 1)
+        // array.splice(number2 - 1, 1)
+        delete inputArray[number - 1]
+        delete inputArray[number2 - 1]
+        let array = inputArray.filter(Boolean)
+        return array
+    }
 
 };
 
