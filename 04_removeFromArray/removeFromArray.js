338ba493ea03d30fdc85cc23c9a32eb0d69d5e65
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-01-29 14:30:07 -0300

    Advanced to test 6, stuck with current code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,4 @@
-const removeFromArray = function(inputArray, number = "", number2 = "", number3 = "") {
+const removeFromArray = function(inputArray, number = "", number2 = "", number3 = "", number4 = "") {
     if (number2 == "") {
         const arrayLength = inputArray.length
         // array.splice(number - 1, 1)
@@ -15,8 +15,25 @@ const removeFromArray = function(inputArray, number = "", number2 = "", number3 
         let array = inputArray.filter(Boolean)
         return array
     }
+    if (number4 == "") {
+        delete inputArray[number -1]
+        delete inputArray[number2 - 1]
+        delete inputArray[number3 - 1]
+        let array = inputArray.filter(Boolean)
+        return array        
+    }
+    else {
+        delete inputArray[number -1]
+        delete inputArray[number2 - 1]
+        delete inputArray[number3 - 1]
+        delete inputArray[number4 - 1]
+        let array = inputArray.filter(Boolean)
+        return array
+    }
 
 };
+
+removeFromArray([1,2,3,4], 1, 2, 3, 4)
 
 // Do not edit below this line
 module.exports = removeFromArray;
