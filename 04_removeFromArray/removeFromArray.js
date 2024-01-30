46e11eb0d2fddc837a33ccb83b16055799810687
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-01-30 11:24:32 -0300

    Full rework, simplified to use only filters. Final

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,39 +1,9 @@
-const removeFromArray = function(inputArray, number = "", number2 = "", number3 = "", number4 = "") {
-    if (number2 == "") {
-        const arrayLength = inputArray.length
-        // array.splice(number - 1, 1)
-        delete inputArray[number - 1]
-        let array = inputArray.filter(Boolean)
-        return array
-    }
-    if (number3 == "") {
-        // const arrayLength = inputArray.length
-        // array.splice(number - 1, 1)
-        // array.splice(number2 - 1, 1)
-        delete inputArray[number - 1]
-        delete inputArray[number2 - 1]
-        let array = inputArray.filter(Boolean)
-        return array
-    }
-    if (number4 == "") {
-        delete inputArray[number -1]
-        delete inputArray[number2 - 1]
-        delete inputArray[number3 - 1]
-        let array = inputArray.filter(Boolean)
-        return array        
-    }
-    else {
-        delete inputArray[number -1]
-        delete inputArray[number2 - 1]
-        delete inputArray[number3 - 1]
-        delete inputArray[number4 - 1]
-        let array = inputArray.filter(Boolean)
-        return array
-    }
-
-};
-
-removeFromArray([1,2,3,4], 1, 2, 3, 4)
-
+const removeFromArray = function(inputArray, input1 = "", input2 = "", input3 = "", input4 = "") {
+var array = inputArray.filter((x) => x !== input1)
+var array = array.filter((x) => x !== input2)
+var array = array.filter((x) => x !== input3)
+var array = array.filter((x) => x !== input4)
+return array
+}
 // Do not edit below this line
 module.exports = removeFromArray;
