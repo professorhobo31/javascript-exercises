1f2b1bc0fbda6d5649573201ad62e3e6e03997d7
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-02-01 11:41:19 -0300

    Add solution for exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,23 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (typeof year !== "number") {
+        return "ERROR"
+    }
+    else if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true   //century but div by 400 is LY
+            }
+            else {
+                return false   //centuries are not LYs
+            }
+        }
+        else {
+            return true   //div by 4 not century is LY
+        }
+    }
+    else {
+        return false   //not div 4 not LY
+    }
 };
 
 // Do not edit below this line
