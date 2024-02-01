ec856a6af6c5dd6a79665c105bd8659d7cadac0b
professorhobo31 <144038113+professorhobo31@users.noreply.github.com>
2024-02-01 12:27:27 -0300

    Add solution to exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputFahrenheit) {
+  let converted = (inputFahrenheit - 32)/(9/5)
+  converted = Number(converted.toFixed(1))
+  return converted
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputCelsius) {
+  let converted = (inputCelsius * (9/5)) + 32
+  converted = Number(converted.toFixed(1))
+  return converted
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
